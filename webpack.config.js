0fdcbb6a01c7a50b97283cd4d88416c11308ddcf
Samer Buna <samer.buna@gmail.com>
2017-06-24 11:24:43 -0700

    Separating Vendor Files

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,34 @@
 const path = require('path');
+const webpack = require('webpack');
 
 const config = {
   resolve: {
-    modules: [
-      path.resolve('./lib'),
-      path.resolve('./node_modules'),
-    ]
+    modules: [path.resolve('./lib'), path.resolve('./node_modules')]
   },
-  entry: ['babel-polyfill', './lib/renderers/dom.js'],
+  entry: {
+    vendor: [
+      'babel-polyfill',
+      'react',
+      'react-dom',
+      'prop-types',
+      'axios',
+      'lodash.debounce',
+      'lodash.pickby'
+    ],
+    app: ['./lib/renderers/dom.js']
+  },
   output: {
     path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
   module: {
-    rules: [
-      {test: /\.js$/, exclude: /node_modules/, use: 'babel-loader'}
-    ]
-  }
+    rules: [{ test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' }]
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    })
+  ]
 };
 
 module.exports = config;
